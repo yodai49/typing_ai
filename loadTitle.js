fd315ae1b5d141c3b5cdfa155ce67c43cf59e748
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2022-02-14 00:15:21 +0900

    Edit Title.

---
+++ loadTitle.js
@@ -0,0 +1,122 @@
+var t = 0; //グローバルタイム 毎ターンperformance.now()を格納
+var loadPfnw=performance.now();//ページが開かれた時間を記録しておく
+const WIDTH = 960;
+const HEIGHT = 540;
+const MAIN_FONTNAME = "JuliusSansOne";
+const JAPANESE_FONTNAME = "Stick";
+const DEBUG_MODE=0;
+const IMG_CNT = 1;//読みこむイメージの総数
+var loadedPfnw=-1,loadingCount=0;
+var SCENE_ANI=400; //ロード終了後のアニメーション時間
+var scene=0;//シーン遷移  0:ローディング画面　1:タイトル画面
+var nextScene=0;//次のシーン
+var sceneAni=0;//シーンのアニメーション
+var ctx2d,ctx2dImg;//キャンバス（メイン）とキャンバス（背景画像）
+var mouseX=0,mouseY=0,clickX=0,clickY=0,mouseStatus=0;
+var backImg= [],imgLoadedCnt=0;//背景イメージ格納用
+
+// ページの読み込みを待つ
+
+window.addEventListener('load', init); //ロードイベント登録
+window.addEventListener('DOMContentLoaded', function(){ //クリックイベント登録
+    ctx2d=document.getElementById("myCanvas").getContext("2d");
+    ctx2dImg=document.getElementById("myCanvas2").getContext("2d");
+    window.addEventListener("keydown", function(e){
+
+    });
+});
+
+function getRGBA(col,T,t,r,g,b){
+    // 色の指定に使う関数　Tは点滅の周期(0で点滅なし)、tはカウント用、固定透明度 colに-1を指定するとr,g,bが有効
+    const COLSET = [[0,0,0], //BLACK
+                    [15,15,25], // DARK GRAY BLUE
+                    [240,240,240],// WHITE
+                    [160,25,15],//DEEP RED
+                    [107,76,0], //黄土色
+                    [111,28,3],//茶色
+                    [83,10,140],//紫色
+                    [255,234,145],//クリーム色
+                    [180,30,20]]; //やや明るい赤
+    if(col>=0){
+        r=COLSET[col][0];
+        g=COLSET[col][1];
+        b=COLSET[col][2];
+    }
+    if(T){
+        return "rgba("  + r + "," + g + "," + b + "," + (0.4+0.3*Math.sin(t/T*Math.PI*2)) + ")";
+    } else{
+        return "rgba("  + r + "," + g + "," + b + ","+t+")";
+    }
+}
+
+function drawLoadingCircle(x,y,size,t,speed){
+    ctx2d.lineWidth=size*0.08;
+    var ani = [];
+    for(var i = 0;i < 5;i++){
+        ani[i] = (t/speed+0.5*Math.sin((t+i*130)/speed))*Math.PI;
+    }
+
+    ctx2d.strokeStyle=getRGBA(3,0,0.8);
+    ctx2d.beginPath();
+    ctx2d.arc(x,y,size,Math.PI*0.3/10+ani[0],Math.PI*1/2+ani[0]);
+    ctx2d.stroke();
+
+    ctx2d.beginPath();
+    ctx2d.strokeStyle=getRGBA(4,0,0.8);
+    ctx2d.arc(x,y,size,Math.PI*(1/2+0.8/10)+ani[1],Math.PI*(1-0.5/10)+ani[1]);
+    ctx2d.stroke();
+
+    ctx2d.beginPath();
+    ctx2d.strokeStyle=getRGBA(5,0,0.8);
+    ctx2d.arc(x,y,size,Math.PI+ani[2],Math.PI*(2-0.2/5)+ani[2]);
+    ctx2d.stroke();
+
+    ctx2d.beginPath();
+    ctx2d.strokeStyle=getRGBA(6,0,0.8);
+    ctx2d.arc(x,y,size*0.84,Math.PI*0.8/5+ani[3],Math.PI*3/5+ani[3]);
+    ctx2d.stroke();
+
+    ctx2d.beginPath();
+    ctx2d.strokeStyle=getRGBA(7,0,0.8);
+    ctx2d.arc(x,y,size*0.84,Math.PI*3.8/5+ani[4],Math.PI*8/5+ani[4]);
+    ctx2d.stroke();
+}
+
+
+function init() {
+    //ローディング処理////////////////////////////////////////
+
+    //2Dの処理
+    ctx2d.width = WIDTH;
+    ctx2d.height = HEIGHT;
+    ctx2d.textBaseLine="top";
+    //画像の格納
+    backImg[0] = new Image();
+    backImg[0].src="./img/back_menu.jpeg";
+    for(var i = 0;i < backImg.length;i++) backImg[i].onload=()=>{imgLoadedCnt++};
+
+    tick();
+
+    function tick() {
+        t=performance.now()-loadPfnw;
+    
+        //リセット処理
+        ctx2d.clearRect(0,0,WIDTH,HEIGHT);
+    
+        ctx2d.fillStyle=getRGBA(0,0,t);
+        ctx2d.fillStyle="rgba(0,0,0,1)";
+        ctx2d.fillRect(0,0,WIDTH,HEIGHT);
+    
+        ctx2d.font="24px " + MAIN_FONTNAME;
+        if(loadingCount){
+            ctx2d.fillStyle=getRGBA(2,0,1);
+            ctx2d.fillText("LOADED!",(WIDTH-ctx2d.measureText("LOADED!").width)/2,HEIGHT/2);
+        } else{
+            ctx2d.fillStyle=getRGBA(2,1000,t);
+            ctx2d.fillText("LOADING",(WIDTH-ctx2d.measureText("LOADING").width)/2,HEIGHT/2);
+        }
+        drawLoadingCircle(WIDTH/2,HEIGHT/2,100,t,1000);
+        if(DEBUG_MODE) SCENE_ANI=100;
+        if(sceneAni || performance.now() - sceneAni < SCENE_ANI*2.5) requestAnimationFrame(tick);
+    }
+}
--- script.js
+++ script.js
@@ -1,47 +1,158 @@
-var t = 0; //time
-var mode = 0; //mode 0タイトル画面　1セレクト画面　2プレイ画面　3リザルト画面　
-var animationmax = 1; //nextmodeにうつるタイミング
-var animationcnt=0; //アニメーションのカウンター 1でスタート -1処理前
-var nextmode=0; //次にうつるモード
-
-// ページの読み込みを待つ
-
-
+var prls = [];//描画する平行四辺形の構造体の配列
 function keypress(mykey,mykeycode){ //キー入力イベント
-    if(mykey=="z"){
-        window.alert("z");
-    }
+
 }
 
 window.addEventListener('load', init); //ロードイベント登録
+window.addEventListener('mousemove',function(e){ //マウスが動いた時に座標をセット
+    mouseX=e.clientX-document.getElementById("myCanvas").getBoundingClientRect().left;
+    mouseY=e.clientY-document.getElementById("myCanvas").getBoundingClientRect().top;
+})
+window.addEventListener('click',function(e){//クリックされた時に座標をセット
+    clickX=mouseX;
+    clickY=mouseY;
+})
 window.addEventListener('DOMContentLoaded', function(){ ///キー入力イベント登録
     window.addEventListener("keydown", function(e){
       keypress(e.key,e.keyCode);
     });
 });
-
+function drawMouseCursor(){ //マウスカーソルを描画する関数
+    ctx2d.strokeStyle=getRGBA(1,600,t);
+    if(mouseStatus) ctx2d.strokeStyle=getRGBA(3,600,t);
+    ctx2d.fillStyle=getRGBA(2,0,0.5);
+    ctx2d.beginPath();
+    ctx2d.lineWidth=3;
+    ctx2d.arc(mouseX,mouseY,10,0,Math.PI*2);
+    ctx2d.stroke();
+    ctx2d.fill();
+}
+function drawPrl(drawPrl){//平行四辺形を描画する関数
+    ctx2d.lineWidth=6;
+    var drawGrad=ctx2d.createLinearGradient(drawPrl.x2,drawPrl.y1,drawPrl.x1,drawPrl.y2);
+    const PRL_COLSET=[[[215,213,228,0.7],[145,143,168,0.7],[225,223,238,0.7]],
+            [[175,173,188,0.7],[125,123,138,0.7],[185,183,208,0.7]]];
+    const FRAME_COLSET=[[20,23,45,0.8],[20,23,45,0.8]];
+    const TEXT_COLSET=[[30,30,30],[30,30,30]];
+    for(var i = 0;i < 3;i++){
+        drawGrad.addColorStop(i/2+0.1*(i==1),'rgba(' + 
+         (PRL_COLSET[drawPrl.hoverColSet][i][0]*drawPrl.hoverCounter+PRL_COLSET[drawPrl.colSet][i][0]*(10-drawPrl.hoverCounter))/10 + ',' +  
+         (PRL_COLSET[drawPrl.hoverColSet][i][1]*drawPrl.hoverCounter+PRL_COLSET[drawPrl.colSet][i][1]*(10-drawPrl.hoverCounter))/10 + ',' +  
+         (PRL_COLSET[drawPrl.hoverColSet][i][2]*drawPrl.hoverCounter+PRL_COLSET[drawPrl.colSet][i][2]*(10-drawPrl.hoverCounter))/10+ ',0.7)')
+    }
+    ctx2d.fillStyle=drawGrad;
+    ctx2d.strokeStyle='rgba(' + 
+    (FRAME_COLSET[drawPrl.hoverColSet][0]*drawPrl.hoverCounter+FRAME_COLSET[drawPrl.colSet][0]*(10-drawPrl.hoverCounter))/10 + ',' +  
+    (FRAME_COLSET[drawPrl.hoverColSet][1]*drawPrl.hoverCounter+FRAME_COLSET[drawPrl.colSet][1]*(10-drawPrl.hoverCounter))/10 + ',' +  
+    (FRAME_COLSET[drawPrl.hoverColSet][2]*drawPrl.hoverCounter+FRAME_COLSET[drawPrl.colSet][2]*(10-drawPrl.hoverCounter))/10+ ',0.8)';
+    ctx2d.beginPath();
+    ctx2d.moveTo((drawPrl.x1+drawPrl.x2)/2,drawPrl.y1);
+    ctx2d.lineTo(drawPrl.x2,drawPrl.y1);
+    ctx2d.lineTo(drawPrl.x2-(drawPrl.y2-drawPrl.y1)*0.3,drawPrl.y2);
+    ctx2d.lineTo(drawPrl.x1,drawPrl.y2);
+    ctx2d.lineTo(drawPrl.x1+(drawPrl.y2-drawPrl.y1)*0.3,drawPrl.y1);
+    ctx2d.lineTo((drawPrl.x1+drawPrl.x2)/2,drawPrl.y1);
+    ctx2d.stroke();
+    ctx2d.fill();
+    ctx2d.fillStyle='rgba(' + 
+    (TEXT_COLSET[drawPrl.hoverColSet][0]*drawPrl.hoverCounter+TEXT_COLSET[drawPrl.colSet][0]*(10-drawPrl.hoverCounter))/10 + ',' +  
+    (TEXT_COLSET[drawPrl.hoverColSet][1]*drawPrl.hoverCounter+TEXT_COLSET[drawPrl.colSet][1]*(10-drawPrl.hoverCounter))/10 + ',' +  
+    (TEXT_COLSET[drawPrl.hoverColSet][2]*drawPrl.hoverCounter+TEXT_COLSET[drawPrl.colSet][2]*(10-drawPrl.hoverCounter))/10+ ',1)';
+    ctx2d.font=(drawPrl.y2-drawPrl.y1)*0.34 + "pt " + JAPANESE_FONTNAME;
+    ctx2d.fillText(drawPrl.text,(drawPrl.x2+drawPrl.x1)/2-ctx2d.measureText(drawPrl.text).width/2,drawPrl.y1+(drawPrl.y2-drawPrl.y1)*0.62);
+}
+function drawTitleScene(){ ///タイトル画面の描画関数
+    ctx2d.fillStyle=getRGBA(1,0,1);//黒背景
+    ctx2d.fillRect(0,0,WIDTH,HEIGHT);
+    drawLoadingCircle(WIDTH-230,230,210,t/3.2,1000);//////////動く丸
+    drawLoadingCircle(150,150,130,t/3,1000);
+    drawLoadingCircle(50,HEIGHT-60,250,t/2.7,1000);
+    ctx2d.font="55px " + MAIN_FONTNAME;//////////タイトル文字
+    ctx2d.fillStyle=getRGBA(2,0,1);
+    ctx2d.fillText("A V A - T Y P E",(WIDTH-ctx2d.measureText("A V A - T Y P E").width)/2,HEIGHT/2-30);
+    ctx2d.font="25px " + JAPANESE_FONTNAME;
+    ctx2d.fillText("アバタイプ",(WIDTH-ctx2d.measureText("アバタイプ").width)/2,HEIGHT/2+40);
+    drawPrl(prls[0]);
+}
+function drawMenu(){
+    ctx2d.fillStyle="rgba(175,175,225,0.2)";
+    ctx2d.fillRect(30+(t/2-(Math.floor(t/2/960)*960)),50,60,10);
+    drawPrl(prls[0]);
+}
+function processMouseEvent(){ //平行四辺形ボタンに対してのホバー処理
+    mouseStatus=0;
+    for(var i = 0;i < prls.length;i++){
+        if(mouseY>prls[i].y1 && mouseY<prls[i].y2 && mouseX>prls[i].x1+(prls[i].y2-prls[i].y1)*0.3 && mouseX<prls[i].x2-(prls[i].y2-prls[i].y1)*0.3){
+            prls[i].hoverCounter++;
+            if(prls[i].hoverCounter>10) prls[i].hoverCounter=10;
+            mouseStatus=1;
+        } else{
+            prls[i].hoverCounter--;
+            if(prls[i].hoverCounter<0) prls[i].hoverCounter=0;
+        }
+    }
+}
+function processClicked(){
+    for(var i = 0;i < prls.length;i++){
+        if(prls[i].hoverCounter && sceneAni==0) {
+            if(clickY>prls[i].y1 && clickY<prls[i].y2 && clickX>prls[i].x1+(prls[i].y2-prls[i].y1)*0.3 && clickX<prls[i].x2-(prls[i].y2-prls[i].y1)*0.3){
+               prls[i].onClick();
+               clickX=0,clickY=0;
+            }
+        }
+    }
+}
+function changeScene(prev,next){ //シーン遷移の関数
+    prls=[];
+    ctx2dImg.clearRect(0,0,WIDTH,HEIGHT);
+    if(next==1){ //タイトル画面に遷移する場合
+        prls.push({x1:WIDTH/2-100,y1:HEIGHT/2+100,x2:WIDTH/2+100,y2:HEIGHT/2+150,colSet:0,hoverColSet:1,hoverCounter:0,text:"START",onClick:function(){nextScene=2;sceneAni=t;}})
+    } if(next == 2){
+        ctx2dImg.drawImage(backImg[0],0,0,WIDTH,HEIGHT);
+        prls.push({x1:100,y1:100,x2:300,y2:250,colSet:0,hoverColSet:1,hoverCounter:0,text:"MENU",onClick:function(){nextScene=1;sceneAni=t;}})
+    }
+}
+function checkLoaded(){
+    if(imgLoadedCnt==IMG_CNT){
+        imgLoadedCnt=-1;
+        sceneAni=performance.now();//ロード完了後にこれを実行
+        nextScene=1;
+    }
+}
 function init() {
     //ローディング処理////////////////////////////////////////
-
-    // サイズを指定
-    const width = 960;
-    const height = 540;
-
-    //2Dの処理
-    ctx2d=document.getElementById("myCanvas2").getContext("2d");
-    ctx2d.width = width;
-    ctx2d.height = height;
 
     tick();
 
     function tick() {
-        t++;//システム系の処理
+        t=performance.now();
+        if(sceneAni==0 && imgLoadedCnt!=-1) checkLoaded();
+
         //2次元のリセット処理
-        ctx2d.clearRect(0,0,width,height);
+        if(scene) ctx2d.clearRect(0,0,WIDTH,HEIGHT);
 
-        ctx2d.fillStyle="rgba(255,255,0,1.0)";
-        ctx2d.fillRect(30+t,30,30,30);
+        if(scene==1){ //タイトル画面
+            drawTitleScene();
+        } else if(scene == 2) { //メニュー画面
+            drawMenu();
+        }
 
+        /////////////////全シーン共通処理
+        drawMouseCursor();
+        processMouseEvent();
+        if(clickX && clickY) processClicked();
+        ///////////////
+        if(sceneAni){
+            if(nextScene!=scene){
+                ctx2d.fillStyle="rgba(0,0,0," + (t-sceneAni)/(SCENE_ANI * (1+(scene==1 || scene==0)))+")";
+                ctx2d.fillRect(0,0,WIDTH,HEIGHT);
+                if(t-sceneAni > SCENE_ANI * (1+(scene==1 || scene==0))) scene=nextScene,sceneAni=t,changeScene(scene,nextScene);
+            } else{
+                ctx2d.fillStyle="rgba(0,0,0," + (1-(t-sceneAni)/SCENE_ANI)+")";
+                ctx2d.fillRect(0,0,WIDTH,HEIGHT);
+                if(t-sceneAni > SCENE_ANI) sceneAni=0;
+            }
+        }
         requestAnimationFrame(tick);
     }
 }
